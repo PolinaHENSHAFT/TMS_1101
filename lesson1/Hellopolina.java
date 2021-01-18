12b4da4dd30f8e4349757670217f088f3b595749
PolinaHenshaft <polinagenshaft@gmail.com>
2021-01-18 17:32:21 +0200

    exaple hello

---
+++ lesson1/Hellopolina.java
@@ -0,0 +1,12 @@
+public class Hellopolina {
+    
+	    public static void main(String ... args) throws Exception {
+	        while(true) {
+	    	System.out.println("Hello polina!!");
+            Thread.sleep(1000);
+             
+        	}
+
+     	}
+
+}
---
+++ lesson1/Helloworld.java
@@ -0,0 +1,12 @@
+public class Helloworld {
+    
+	    public static void main(String ... args) throws Exception {
+	        while(true) {
+	    	System.out.println("Hello TMS group World!");
+            Thread.sleep(1000);
+             
+        	}
+
+     	}
+
+}
